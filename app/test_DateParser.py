2f1e22ba5e635c21ae16473a22c3c2f50b2d55f4
Akash127 <mail.akashsharma@gmail.com>
2018-03-08 23:00:33 -0700

    Update test_DateParser.py

--- app/test_DateParser.py
+++ app/test_DateParser.py
@@ -7,7 +7,7 @@ class TestTickets(unittest.TestCase):
 
 	def test_parse(self):
 		date = "2018-03-03T00:15:44Z"
-		self.assertEqual(DateParser().parse(date), "Date: 3/3/2018 Time: 0:15:44")
+		self.assertEqual(DateParser().parse(date), "Date: 3/3/2018 Time: 0:15:44 UTC")
 
 
 
